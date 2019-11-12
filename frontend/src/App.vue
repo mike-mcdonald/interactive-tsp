cd04d9007898d8130edcb21c31c1434cbb501fd3
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-12 09:10:47 -0800

    TSP-75 Update links in navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <header class="p-2 lg:py-0 mx-auto flex flex-wrap items-center text-black border-b border-black" role="banner">
+      <router-link to="/" class="mr-3">
+        <Logo class="w-24"></Logo>
+      </router-link>
+      <router-link to="/" class="mr-3 text-xl">Transportation system plan</router-link>
+      <nav class="-mx-2 flex flex-row flex-wrap">
+        <router-link to="/streets" class="py-2 px-2 md:py-4 hover:bg-fog-500" active-class="bg-fog-500">
+          <span class="border-b-2 border-black">Streets</span>
+        </router-link>
+      </nav>
+    </header>
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue';
 
+import Logo from 'portland-pattern-lab/source/_patterns/01-atoms/04-images/Logo.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Logo
+  }
+});
+</script>
 <style lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,700,800');
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  @apply font-sans text-sm text-black;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+@screen md {
+  #app {
+    @apply text-base;
   }
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './router/index';
+import store from './store/index';
 import './registerServiceWorker';
+import './worker-config';
 
 Vue.config.productionTip = false;
 
