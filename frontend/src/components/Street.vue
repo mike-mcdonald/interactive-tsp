47e27dbe19e68c4dff0062beef4268a4d48249ce
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-19 13:46:00 -0800

    TSP-79 Implement components for street routing

--- frontend/src/components/Street.vue
+++ frontend/src/components/Street.vue
@@ -1,8 +1,7 @@
 <template>
-  <article class="px-2" v-if="street">
+  <article class="p-2">
     <h1 class="mb-3 text-3xl lg:text-4xl">{{ street.name }}</h1>
-    <p class="mb-3 text-2xl" v-if="street.block">{{ street.block }}
-    block</p>
+    <p class="mb-3 text-2xl" v-if="street.block">{{ street.block }} block</p>
     <div class="flex flex-wrap items-center mb-3">
       <dl>
         <div class="flex flex-wrap items-center">
@@ -11,40 +10,106 @@
         </div>
       </dl>
     </div>
-    <h2 class="mb-3 text-2xl lg:text-3xl">Classifications</h2>
-    <dl>
-      <div v-for="(classification, index) in Object.keys(street.classifications)" :key="index" class="flex flex-wrap items-center mb-2">
-        <dt>{{ classification.charAt(0).toUpperCase() + classification.slice(1) }} classification:</dt>
-        <dd class="ml-2">
-          <a href="#" class="border-b-2 border-black">{{ street.classifications[classification] }}</a>
-        </dd>
-      </div>
-    </dl>
-    <h2 class="mb-3 text-2xl lg:text-3xl">Projects affecting this street</h2>
-    <ul class="list-none m-0">
-      <li class="my-2" v-for="project in street.projects" :key="project.id">
-        <a href="#" class="border-b-2 border-black">{{ project.name }}</a>
-      </li>
-    </ul>
+    <transition name="fade">
+      <section v-if="street.classifications">
+        <h2 class="mb-3 text-2xl lg:text-3xl">Classifications</h2>
+        <dl>
+          <div
+            v-for="(classification, index) in classificationKeys()"
+            :key="index"
+            class="flex flex-wrap items-center mb-2"
+          >
+            <dt>{{ classification.charAt(0).toUpperCase() + classification.slice(1) }} classification:</dt>
+            <dd class="ml-2">{{ street.classifications[classification] }}</dd>
+          </div>
+        </dl>
+      </section>
+    </transition>
+
+    <transition name="fade">
+      <section v-if="street.projects && street.projects.length > 0">
+        <h2 class="mb-3 text-2xl lg:text-3xl">Projects affecting this street</h2>
+        <ul class>
+          <li class="my-2 p-2 rounded border" v-for="project in street.projects" :key="project.id">
+            <router-link :to="`/projects/${project.id}`">
+              <h3 class="mb-1">{{ project.name }}</h3>
+              <p class="text-xs">{{ project.description }}</p>
+              <div class="flex flex-row flex-wrap -mx-2 text-xs text-gray-600">
+                <span class="mx-2 flex flex-row items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="w-3 h-3 mr-1"
+                  >
+                    <line x1="12" y1="1" x2="12" y2="23" />
+                    <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
+                  <span>{{ project.estimatedCost.toLocaleString() }}</span>
+                </span>
+                <span class="mx-2 flex flex-row items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="w-3 h-3 mr-1"
+                  >
+                    <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+                    <line x1="16" y1="2" x2="16" y2="6" />
+                    <line x1="8" y1="2" x2="8" y2="6" />
+                    <line x1="3" y1="10" x2="21" y2="10" />
+                  </svg>
+
+                  <span>{{ project.estimatedTimeframe }}</span>
+                </span>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </section>
+    </transition>
   </article>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 import { mapActions, mapState } from 'vuex';
 
+import { Street, StreetState } from '@/store/streets/types';
+
 export default Vue.extend({
   name: 'Street',
-  computed: {
-    ...mapState('streets', {
-      street: (state: any) => state.selected
-    })
+  props: {
+    street: {
+      type: Object,
+      required: true
+    }
   },
   methods: {
-    ...mapActions('streets', ['selectStreet']),
-
-  },
-  mounted: function () {
-    this.selectStreet(this.$route.params.id);
+    classificationKeys: function() {
+      return this.street && this.street.classifications ? Object.keys(this.street.classifications) : [];
+    }
   }
-})
+});
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
