ac376f29bf42650b024361b5b3ec6830e0836809
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2019-12-05 17:02:52 -0800

    TSP-82 Add view component for text

---
+++ frontend/src/views/Text.vue
@@ -0,0 +1,100 @@
+<template>
+  <main class="container mx-auto my-4 flex flex-col md:flex-row-reverse justify-between">
+    <aside class="md:w-1/3 px-2">
+      <nav class="md:sticky md:top-10 md:overflow-y-auto md:max-h-(screen-16)">
+        <ol class="list-none">
+          <listing
+            v-for="section in sections"
+            :key="section.id"
+            :id="section.id"
+            :name="section.name"
+            :depth="section.depth"
+            :sections="section.sections"
+          />
+        </ol>
+      </nav>
+    </aside>
+    <article class="md:w-2/3 px-2">
+      <text-section
+        v-for="section in sections"
+        :key="section.id"
+        :id="section.id"
+        :content="section.content"
+        :sections="section.sections"
+      />
+    </article>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import TextSection from '@/components/text/Section.vue';
+import Listing from '@/components/text/Listing.vue';
+import { mapState, mapActions } from 'vuex';
+
+const TIMEOUT = 1;
+
+export default Vue.extend({
+  name: 'TextView',
+  components: {
+    TextSection,
+    Listing
+  },
+  computed: {
+    ...mapState('text', ['sections'])
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.$store.dispatch('text/findText');
+    });
+  }
+});
+</script>
+
+<style lang="scss">
+article {
+  h1 {
+    @apply text-3xl mb-6;
+  }
+
+  h2 {
+    @apply text-2xl mb-5;
+  }
+
+  h3 {
+    @apply text-xl mb-4;
+  }
+
+  h4 {
+    @apply text-lg mb-3;
+  }
+
+  h5 {
+    @apply text-base mb-2;
+  }
+
+  h6 {
+    @apply text-sm mb-1;
+  }
+
+  p {
+    @apply mb-3;
+  }
+}
+
+@variants hover, focus {
+  .translate-r-2px {
+    transform: translateX(2px) !important;
+  }
+  .translate-t-2px {
+    transform: translateY(-2px) !important;
+  }
+  .translate-tr-2px {
+    transform: translateX(2px) translateY(-2px) !important;
+  }
+  .transition-fast {
+    transition: all 0.2s ease !important;
+  }
+}
+</style>
