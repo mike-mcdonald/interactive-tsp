3b33ae6583c174d6b7df70d6817850f4dfd38153
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2020-01-06 18:04:43 -0800

    TSP-41 Add filterinf and index searching

--- frontend/src/store/projects/mutations.ts
+++ frontend/src/store/projects/mutations.ts
@@ -1,5 +1,5 @@
 import { MutationTree } from 'vuex';
-import { ProjectState, Project } from './types';
+import { ProjectState, Project, ProjectFilter } from './types';
 
 export const mutations: MutationTree<ProjectState> = {
   clearProjects(state) {
@@ -11,7 +11,25 @@ export const mutations: MutationTree<ProjectState> = {
     }
     state.list.push(...projects);
   },
+  setFilter(state, filter: ProjectFilter) {
+    state.filter = filter;
+  },
+  addTimeframe(state, timeframe: string) {
+    const idx = state.filter.timeframes?.indexOf(timeframe);
+    if (idx && idx == -1) {
+      state.filter.timeframes?.push(timeframe);
+    }
+  },
+  removeTimeframe(state, timeframe: string) {
+    const idx = state.filter.timeframes?.indexOf(timeframe);
+    if (idx && idx > -1) {
+      state.filter.timeframes?.splice(idx, 1);
+    }
+  },
   setSelectedProjects(state, projects: Project[]) {
     state.selected = projects;
+  },
+  setIndex(state, index: lunr.Index) {
+    state.index = index;
   }
 };
