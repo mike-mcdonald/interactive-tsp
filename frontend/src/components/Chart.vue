a9636c68d3053e192263ecf2806ff36875bb5d3f
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-02-21 15:51:16 -0800

    TSP-33 Add a simple "chart" component

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="relative">
+    <div ref="chart" class="flex flex-row">
+      <router-link
+        v-for="entry in dataset"
+        :key="entry.classificationValue"
+        class="h-4"
+        :style="{
+          'background-color': entry.color.formatRgb(),
+          width: `${(entry.count / total) * 100}%`
+        }"
+        :to="{
+          name: 'text',
+          hash: `#${entry.label
+            .toLowerCase()
+            .split(' ')
+            .join('-')}`
+        }"
+        :title="`${entry.label}: ${entry.count} / ${total}`"
+        @mouseenter.native="show[entry.classificationValue] = true"
+        @mouseleave.native="show[entry.classificationValue] = false"
+        @touchenter.native="show[entry.classificationValue] = true"
+        @touchleave.native="show[entry.classificationValue] = false"
+      ></router-link>
+    </div>
+    <div
+      v-for="entry in dataset"
+      :key="entry.classificationValue"
+      v-show="show[entry.classificationValue]"
+      class="absolute z-50 inset-x-0 top-0 mt-6 px-2 py-1 bg-white border border-black rounded"
+    >{{ entry.label }}: {{ entry.count }} / {{ total }}</div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+import { ClassificationAnalysisData } from '../store/streets/types';
+
+export default Vue.extend({
+  name: 'Chart',
+  props: {
+    dataset: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    const show = this.dataset.reduce((prev: any, curr: any) => {
+      prev[curr.classificationValue] = false;
+      return prev;
+    }, {});
+
+    return {
+      show
+    };
+  },
+  computed: {
+    total() {
+      return this.dataset.reduce((prev: any, curr: any) => {
+        prev = prev + curr.count;
+        return prev;
+      }, 0);
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
