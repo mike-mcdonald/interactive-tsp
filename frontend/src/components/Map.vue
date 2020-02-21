a338fa8f101146c03d95dadd596f681d932049ca
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-02-21 15:51:41 -0800

    TSP-33 Add titles to buttons on UI

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -6,7 +6,11 @@
     <div ref="top-left">
       <slot name="top-left">
         <div class="flex flex-col">
-          <button class="p-2 bg-white border border-fog-900" v-on:click="incrementZoom">
+          <button
+            class="p-2 bg-white border border-fog-900"
+            v-on:click="incrementZoom"
+            title="Zoom in"
+          >
             <!-- This is from feather.  Their site is here: https://www.feathericons.com -->
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +28,11 @@
               <line x1="5" y1="12" x2="19" y2="12" />
             </svg>
           </button>
-          <button class="p-2 bg-white border border-fog-900" v-on:click="decrementZoom">
+          <button
+            class="p-2 bg-white border border-fog-900"
+            v-on:click="decrementZoom"
+            title="Zoom out"
+          >
             <!-- This is from feather.  Their site is here: https://www.feathericons.com -->
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -46,51 +54,12 @@
     </div>
     <div ref="top-right">
       <slot name="top-right">
-        <button v-if="!showSettings" class="p-2 bg-white border border-fog-900" v-on:click="showSettings = true">
-          <!-- This is from feather.  Their site is here: https://www.feathericons.com -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-5 w-5"
-          >
-            <polygon points="12 2 2 7 12 12 22 7 12 2" />
-            <polyline points="2 17 12 22 22 17" />
-            <polyline points="2 12 12 17 22 12" />
-          </svg>
-        </button>
-        <div v-if="showSettings" class="bg-white h-64 overflow-y-auto border border-fog-900">
-          <header class="flex items-baseline justify-between sticky top-0 z-10 bg-gray-100 border-b border-black">
-            <h2 class="p-2 text-2xl">Settings</h2>
-            <button aria-label="Close" class="p-3" @click.stop.prevent="showSettings = false">
-              <X />
-            </button>
-          </header>
-          <main class="overflow-y-auto p-2">
-            <header class="flex items-baseline justify-between">
-              <h3 class="text-xl">Layers</h3>
-            </header>
-            <checkbox
-              v-for="layer in layers"
-              :value="layer.visible"
-              :key="layer.id"
-              :title="layer.title"
-              @input="toggleLayerVisibility(layer.id, $event)"
-            />
-          </main>
-        </div>
-      </slot>
-    </div>
-    <div ref="bottom-left">
-      <slot name="bottom-left"></slot>
-    </div>
-    <div ref="bottom-right">
-      <slot name="bottom-right">
-        <button v-if="!showLegend" class="p-2 bg-white border border-fog-900" v-on:click="showLegend = true">
+        <button
+          v-if="!showLegend"
+          class="p-2 bg-white border border-fog-900"
+          v-on:click="showLegend = true"
+          title="Show legend"
+        >
           <!--
             This is from feather.  Their site is here: https://www.feathericons.com
             Their license is here: https://github.com/feathericons/feather/blob/master/LICENSE
@@ -116,15 +85,28 @@
           </svg>
         </button>
         <div v-show="showLegend" class="bg-white h-64 overflow-y-auto border border-fog-900">
-          <header class="flex items-baseline justify-between sticky top-0 z-10 bg-gray-100 border-b border-black">
+          <header
+            class="flex items-baseline justify-between sticky top-0 z-10 bg-gray-100 border-b border-black"
+          >
             <h2 class="p-2 text-2xl">Legend</h2>
-            <button aria-label="Close" class="p-3" @click.stop.prevent="showLegend = false">
+            <button
+              aria-label="Close"
+              class="p-3"
+              @click.stop.prevent="showLegend = false"
+              title="Close panel"
+            >
               <X />
             </button>
           </header>
           <main ref="legend"></main>
         </div>
       </slot>
+    </div>
+    <div ref="bottom-left">
+      <slot name="bottom-left"></slot>
+    </div>
+    <div ref="bottom-right">
+      <slot name="bottom-right"></slot>
     </div>
   </div>
 </template>
@@ -147,14 +129,13 @@ import SimpleLineSymbol from 'esri/symbols/SimpleLineSymbol';
 import MapView from 'esri/views/MapView';
 import Legend from 'esri/widgets/Legend';
 
-import Checkbox from 'portland-pattern-lab/source/_patterns/02-molecules/form/Checkbox.vue';
 import X from 'portland-pattern-lab/source/_patterns/01-atoms/04-images/X.vue';
 import { MapState } from '../store/map/types';
+import { BBox } from '@turf/helpers';
 
 export default Vue.extend({
   name: 'Map',
   components: {
-    Checkbox,
     X
   },
   props: {
