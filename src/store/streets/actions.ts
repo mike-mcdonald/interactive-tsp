0fc826957e6174d29a65409c368504f6efd374fb
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-03-13 13:55:19 -0700

    Add contextual zooming

--- src/store/streets/actions.ts
+++ src/store/streets/actions.ts
@@ -4,12 +4,14 @@ import { RootState } from '../types';
 
 import area from '@turf/area';
 import bboxPolygon from '@turf/bbox-polygon';
+import length from '@turf/length';
 import axios from 'axios';
 import proj4 from 'proj4';
 import { Extent } from 'esri/geometry';
 
 import { esriGraphics } from '../utils';
 import bbox from '@turf/bbox';
+import { feature } from '@turf/helpers';
 
 // ESRI maps use this wkid
 proj4.defs('102100', proj4.defs('EPSG:3857'));
@@ -19,23 +21,44 @@ export const actions: ActionTree<StreetState, RootState> = {
     let { xmin, ymin, xmax, ymax } = extent;
 
     if (extent.spatialReference.wkid != 4326) {
-      [xmin, ymin] = proj4(extent.spatialReference.wkid.toString(), 'EPSG:4326', [xmin, ymin]);
-      [xmax, ymax] = proj4(extent.spatialReference.wkid.toString(), 'EPSG:4326', [xmax, ymax]);
+      [xmin, ymin] = proj4(
+        extent.spatialReference.wkid.toString(),
+        'EPSG:4326',
+        [xmin, ymin]
+      );
+      [xmax, ymax] = proj4(
+        extent.spatialReference.wkid.toString(),
+        'EPSG:4326',
+        [xmax, ymax]
+      );
     }
 
     if (area(bboxPolygon([xmin, ymin, xmax, ymax])) > 1250000) {
-      commit('setMessages', [{ type: 'info', text: 'Zoom in or search for an address to see available streets...' }], {
-        root: true
-      });
+      commit(
+        'setMessages',
+        [
+          {
+            type: 'info',
+            text: 'Zoom in or search for an address to see available streets...'
+          }
+        ],
+        {
+          root: true
+        }
+      );
       return;
     }
 
-    commit('setMessages', [{ type: 'info', text: 'Retrieving streets...' }], { root: true });
+    commit('setMessages', [{ type: 'info', text: 'Retrieving streets...' }], {
+      root: true
+    });
 
     axios
-      .get<{ errors?: any[]; data: { streets?: Street[] } }>(rootState.graphqlUrl, {
-        params: {
-          query: `{
+      .get<{ errors?: any[]; data: { streets?: Street[] } }>(
+        rootState.graphqlUrl,
+        {
+          params: {
+            query: `{
           streets(bbox:[${xmin},${ymin},${xmax},${ymax}], spatialReference:4326){
             id
             name
@@ -56,15 +79,16 @@ export const actions: ActionTree<StreetState, RootState> = {
             }
           }
         }`.replace(/\s+/g, ' ')
+          }
         }
-      })
+      )
       .then(res => {
         commit('setMessages', undefined, { root: true });
 
         if (res.data.errors) {
           commit(
             'setMessages',
-            [{ type: 'info', text: 'Zoom in or search for an address to see available streets...' }],
+            [{ type: 'warning', text: 'Some data may contain errors...' }],
             { root: true }
           );
         }
@@ -72,7 +96,7 @@ export const actions: ActionTree<StreetState, RootState> = {
         if (res.data.data.streets) {
           commit('setList', []);
           // sort by name then block number
-          let streets = res.data.data.streets.sort(function(a, b) {
+          let streets = res.data.data.streets.sort(function (a, b) {
             var nameA = a.name?.toUpperCase(); // ignore upper and lowercase
             var nameB = b.name?.toUpperCase(); // ignore upper and lowercase
 
@@ -87,39 +111,32 @@ export const actions: ActionTree<StreetState, RootState> = {
             }
 
             // names must be equal
-            return (a.block || Number.MAX_SAFE_INTEGER) - (b.block || Number.MIN_SAFE_INTEGER);
+            return (
+              (a.block || Number.MAX_SAFE_INTEGER) -
+              (b.block || Number.MIN_SAFE_INTEGER)
+            );
           });
 
           commit('setList', streets);
         }
       })
       .catch(() => {
-        commit('setMessages', [{ type: 'error', text: 'Error retrieving streets!' }], { root: true });
+        commit(
+          'setMessages',
+          [{ type: 'error', text: 'Error retrieving streets!' }],
+          { root: true }
+        );
       });
-  },
-  selectStreetById({ commit, dispatch, state }, id: string) {
-    commit('setMessages', undefined, { root: true });
-    let street: Street = { id };
-
-    if (state.list) {
-      street = Object.assign(
-        street,
-        state.list.find(street => {
-          return street.id === id;
-        })
-      );
-    }
-    commit('setSelectedStreet', street);
-
-    dispatch('selectStreet', street);
   },
   selectStreet({ commit, dispatch, rootState }, street: Street) {
     commit('setMessages', undefined, { root: true });
     dispatch('highlightStreet', { street, move: false });
     axios
-      .get<{ errors?: any[]; data: { street?: Street } }>(rootState.graphqlUrl, {
-        params: {
-          query: `{
+      .get<{ errors?: any[]; data: { street?: Street } }>(
+        rootState.graphqlUrl,
+        {
+          params: {
+            query: `{
           street(id:"${street ? street.id : ''}"){
             id
             ${street.name ? '' : 'name'}
@@ -138,7 +155,7 @@ export const actions: ActionTree<StreetState, RootState> = {
               traffic
               greenscape
             }`
-            }
+              }
             ${
               street.projects
                 ? ''
@@ -150,34 +167,49 @@ export const actions: ActionTree<StreetState, RootState> = {
               estimatedCost
               estimatedTimeframe
             }`
-            }
+              }
           }
         }`.replace(/\s+/g, ' ')
+          }
         }
-      })
+      )
       .then(res => {
         if (res.data.errors) {
-          commit('setMessages', [{ type: 'warning', text: 'Some data may contain errors...' }], { root: true });
+          commit(
+            'setMessages',
+            [{ type: 'warning', text: 'Some data may contain errors...' }],
+            { root: true }
+          );
         }
         let data = res.data.data;
         if (data.street) {
           data.street = Object.assign(data.street, street);
           commit('setSelectedStreet', data.street);
-          dispatch('highlightStreet', { street: data.street, move: false });
+          dispatch('highlightStreet', { street: data.street, move: true });
         }
       })
       .catch(() => {
-        commit('setMessages', [{ type: 'error', text: 'Error retrieving the selected street!' }], { root: true });
+        commit(
+          'setMessages',
+          [{ type: 'error', text: 'Error retrieving the selected street!' }],
+          { root: true }
+        );
       });
   },
-  highlightStreet({ commit, rootState }, { street, move }: { street: Street; move: boolean }) {
+  highlightStreet(
+    { commit },
+    { street, move }: { street: Street; move: boolean }
+  ) {
     if (street.geometry) {
-      const graphics = esriGraphics(street.geometry);
+      let graphics = esriGraphics(street.geometry);
       commit('map/setGraphics', graphics, { root: true });
       if (move) {
-        const target: any = { target: graphics };
-        if (area(bboxPolygon(bbox(street.geometry))) < 5000) {
-          //target.zoom = rootState.map?.zoom.focus;
+        const target: any = {
+          target: graphics
+        };
+        const l = length(feature(street.geometry), { units: 'meters' });
+        if (l < 500) {
+          target.zoom = 11;
         }
         commit('map/goTo', target, { root: true });
       }
--- src/store/streets/index.ts
+++ src/store/streets/index.ts
@@ -16,19 +16,49 @@ const namespaced: boolean = true;
 const models = new Array<ViewModel>();
 
 const districts = new Map([
-  ['pedestrian', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/16'],
-  ['bicycle', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/13'],
-  ['freight', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/20']
+  [
+    'pedestrian',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/16'
+  ],
+  [
+    'bicycle',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/13'
+  ],
+  [
+    'freight',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/20'
+  ]
 ]);
 
 const classifications = new Map([
-  ['pedestrian', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/15'],
-  ['bicycle', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/12'],
-  ['transit', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/3'],
-  ['freight', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/19'],
-  ['design', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/10'],
-  ['emergency', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/7'],
-  ['traffic', 'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/4']
+  [
+    'pedestrian',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/15'
+  ],
+  [
+    'bicycle',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/12'
+  ],
+  [
+    'transit',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/3'
+  ],
+  [
+    'freight',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/19'
+  ],
+  [
+    'design',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/10'
+  ],
+  [
+    'emergency',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/7'
+  ],
+  [
+    'traffic',
+    'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/4'
+  ]
 ]);
 
 Promise.all(
@@ -76,7 +106,9 @@ Promise.all(
               layer: new FeatureLayer({
                 url: value[1],
                 outFields: ['*'],
-                definitionExpression: `${value[0]} = '${info.value.toString()}'`,
+                definitionExpression: `${
+                  value[0]
+                  } = '${info.value.toString()}'`,
                 visible: false
               })
             };
