ca2665ba38011a03084c36536e7013c2ddb43bdc
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2020-03-17 12:55:54 -0700

    Update messages to work with purgecss

--- src/components/message/Item.vue
+++ src/components/message/Item.vue
@@ -1,12 +1,5 @@
 <template>
-  <li
-    class="mb-2 px-2 py-3 w-full border border-l-8 rounded"
-    :class="[
-      `border-${colorMap.get(item.type)}-800`,
-      `bg-${colorMap.get(item.type)}-100`,
-      `text-${colorMap.get(item.type)}-900`
-    ]"
-  >
+  <li class="mb-2 px-2 py-3 w-full border border-l-8 rounded" :class="colors">
     {{ item.text }}
   </li>
 </template>
@@ -19,14 +12,19 @@ export default Vue.extend({
   props: {
     item: Object
   },
-  data() {
-    return {
-      colorMap: new Map([
-        ['error', 'red'],
-        ['warning', 'tangerine'],
-        ['info', 'blue']
-      ])
-    };
+  computed: {
+    colors() {
+      switch (this.item.type) {
+        case 'error':
+          return ['border-red-800', 'bg-red-100', 'text-red-900'];
+        case 'warning':
+          return ['border-orange-800', 'bg-orange-100', 'text-orange-900'];
+        case 'info':
+          return ['border-blue-800', 'bg-blue-100', 'text-blue-900'];
+        default:
+          return [];
+      }
+    }
   }
 });
 </script>
