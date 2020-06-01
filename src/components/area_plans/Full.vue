73b03f8ea2267c881637a670e2ddcf8f614c992a
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-06-01 11:11:42 -0700

    TSP-42 Add area plan components

---
+++ src/components/area_plans/Full.vue
@@ -0,0 +1,33 @@
+<template>
+  <article>
+    <h1 class="mb-3 text-3xl">{{ plan.name }}</h1>
+    <field-list :fields="fields" />
+  </article>
+</template>
+
+<script>
+import FieldList from '@/components/fields/List.vue';
+
+export default {
+  name: 'AreaPlan',
+  props: {
+    plan: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    FieldList
+  },
+  computed: {
+    fields() {
+      return ['manager', 'requirements', 'adopted', 'document'].map(key => {
+        return {
+          name: key.charAt(0).toUpperCase() + key.slice(1),
+          value: this.plan[key]
+        };
+      });
+    }
+  }
+};
+</script>
---
+++ src/components/area_plans/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link
+    :to="`${plan.slug}`"
+    :append="$route.params.slug ? false : true"
+    class="block my-2 px-2 py-3 shadow border rounded bg-white hover:bg-blue-100 focus:bg-blue-100"
+    @mouseover.native="highlightPlan({ plan })"
+    @focus.native="highlightPlan({ plan })"
+  >
+    <span class="px-2 py-1 mb-2 bg-fog-300 text-fog-900 text-sm rounded-md shadow inline-block">Area plan</span>
+    <h3 class="text-lg">{{ plan.name }}</h3>
+  </router-link>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  name: 'AreaPlan',
+  props: {
+    plan: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapActions('areaPlans', ['highlightPlan', 'selectPlan'])
+  }
+};
+</script>
