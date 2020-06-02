4c294707e33a5df3498731ffef2ae7ddc4c30f31
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-06-02 14:21:24 -0700

    TSP-42 Update street component to display plans

--- src/components/Street.vue
+++ src/components/Street.vue
@@ -35,18 +35,6 @@
             <span v-else>{{ classificationLabel(classification, street.classifications[classification]) }}</span>
           </dd>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 my-3">
-          <dt class="text-gray-700">Master Street Plans</dt>
-          <dd>
-            <ul class="list-none">
-              <li v-for="plan in street.masterStreetPlans" :key="plan.id">
-                <router-link :to="`/master_street_plans/${plan.id}`" class="border-b-2 border-current">
-                  {{ plan.label }}
-                </router-link>
-              </li>
-            </ul>
-          </dd>
-        </div>
       </dl>
     </section>
     <transition name="fade">
@@ -106,6 +94,23 @@
         </ul>
       </section>
     </transition>
+    <section v-if="plans && plans.length > 0">
+      <h2 class="mb-3 text-2xl lg:text-3xl">Plans near this street</h2>
+      <ul class>
+        <li v-for="plan in plans" :key="plan.id">
+          <router-link
+            :to="`/${plan.type === 'master' ? 'master_street_plans' : 'area_plans'}/${plan.id}`"
+            class="block my-2 px-2 py-3 shadow border rounded bg-white hover:bg-blue-100 focus:bg-blue-100"
+          >
+            <span class="px-2 py-1 mb-2 bg-fog-300 text-fog-900 text-sm rounded-md shadow inline-block"
+              >{{ plan.type === 'master' ? 'Master street' : 'Area' }} plan
+            </span>
+            <h3>{{ plan.name }}</h3>
+            <p class="my-2 text-sm">{{ plan.description }}</p>
+          </router-link>
+        </li>
+      </ul>
+    </section>
   </article>
 </template>
 <script lang="ts">
@@ -113,6 +118,8 @@ import Vue from 'vue';
 import { mapActions, mapState, mapGetters } from 'vuex';
 
 import { Street, StreetState } from '@/store/streets/types';
+import { AreaPlan } from '../store/area_plans/types';
+import { MasterStreetPlan } from '../store/master_street_plans/types';
 
 export default Vue.extend({
   name: 'Street',
@@ -123,7 +130,44 @@ export default Vue.extend({
     }
   },
   computed: {
-    ...mapGetters('streets', ['classificationLabel', 'classificationColor'])
+    ...mapGetters('streets', ['classificationLabel', 'classificationColor']),
+    plans() {
+      const plans: Array<any> = [
+        ...this.street.masterStreetPlans.map((plan: MasterStreetPlan) => {
+          const { label, id } = plan;
+          return {
+            id,
+            name: label,
+            type: 'master'
+          };
+        }),
+        ...this.street.areaPlans.map((plan: AreaPlan) => {
+          const { name, slug } = plan;
+          return {
+            id: slug,
+            name,
+            type: 'area'
+          };
+        })
+      ].sort((a, b) => {
+        var nameA = (a.label || a.name)?.toUpperCase(); // ignore upper and lowercase
+        var nameB = (b.label || b.name)?.toUpperCase(); // ignore upper and lowercase
+
+        if (!nameA || !nameB) {
+          return Number.MAX_SAFE_INTEGER;
+        }
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        // names must be equal
+        return 0;
+      });
+      return plans;
+    }
   },
   methods: {
     classificationKeys: function() {
