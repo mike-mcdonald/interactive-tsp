78c3b1749ddc3b113017d8a7dc36792279c4dd27
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-06-24 17:52:16 -0700

    TSP-35 Add fields to views of plans

--- src/components/master_street_plans/Full.vue
+++ src/components/master_street_plans/Full.vue
@@ -1,6 +1,7 @@
 <template>
   <article>
-    <h1 class="mb-3 text-3xl">{{ plan.label }}</h1>
+    <h1 class="mb-3 text-3xl">{{ plan.name }}</h1>
+    <section class="text-xl text-gray-800">{{ plan.description }}</section>
     <dl>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 my-3">
         <dt class="text-gray-700">Documentation</dt>
@@ -13,9 +14,9 @@
     </dl>
     <h2 class="mb-3 text-2xl">Features of the plan</h2>
     <ul v-if="plan.features.length > 0" class="list-none">
-      <li v-for="feature in plan.features" :key="feature.name" class="my-2 flex items-center justify-between">
-        <toggle :id="feature.name" :value="feature.enabled" @changed="toggleFeature(feature, $event)" class="mr-2" />
-        <label :id="`${feature.name}-label`" class="flex-1 pr-2">{{ feature.label }}</label>
+      <li v-for="feature in plan.features" :key="feature.id" class="my-2 flex items-center justify-between">
+        <toggle :id="feature.id" :value="feature.enabled" @changed="toggleFeature(feature, $event)" class="mr-2" />
+        <label :id="`${feature.id}-label`" class="flex-1 pr-2">{{ feature.type }} {{ feature.alignment }}</label>
         <span class="px-2 py-1 bg-blue-500 text-white text-sm rounded-md">{{ feature.count }}</span>
       </li>
     </ul>
--- src/components/master_street_plans/List.vue
+++ src/components/master_street_plans/List.vue
@@ -7,23 +7,59 @@
     @focus.native="highlightPlan({ plan })"
   >
     <slot>
-      <span class="px-2 py-1 mb-2 bg-fog-300 text-fog-900 text-sm rounded-md shadow inline-block"
-        >Master street plan
-      </span>
-      <h3 class="text-lg">{{ plan.label }}</h3>
+      <article>
+        <header v-if="showType" class="px-2 py-1 mb-2 bg-fog-300 text-fog-900 text-sm rounded-md shadow inline-block">
+          Master street plan
+        </header>
+        <h3 class="text-lg">{{ plan.name }}</h3>
+        <main class="text-sm text-gray-700">
+          <dl>
+            <div class="flex items-center my-1">
+              <dt class="mr-2">
+                <i v-html="feather.icons['file-text'].toSvg({ class: 'w-4 h-4' })" />
+                <span class="sr-only">Documentation</span>
+              </dt>
+              <dd>
+                <a :href="plan.document" target="_blank" class="border-b-2 border-current">
+                  {{ plan.document }}
+                </a>
+              </dd>
+            </div>
+            <div class="flex items-center my-1">
+              <dt class="mr-2">
+                <i v-html="feather.icons['map-pin'].toSvg({ class: 'w-4 h-4' })" />
+                <span class="sr-only">Feature count</span>
+              </dt>
+              <dd>
+                <span>{{ plan.features.length }} features</span>
+              </dd>
+            </div>
+          </dl>
+        </main>
+      </article>
     </slot>
   </router-link>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
+
+import feather from 'feather-icons';
+
 export default {
   name: 'MasterStreetPlan',
   props: {
     plan: {
       type: Object,
       required: true
+    },
+    showType: {
+      type: Boolean,
+      default: false
     }
+  },
+  data() {
+    return { feather };
   },
   methods: {
     ...mapActions('masterStreetPlans', ['highlightPlan', 'selectPlan'])
