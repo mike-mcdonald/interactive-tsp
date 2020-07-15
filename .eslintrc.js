6338b72299e5f9d10fc4288ab16a0e983fa4df41
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-07-15 15:26:54 -0700

    Update prettier plugins according to prettier docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   plugins: ['vue', '@typescript-eslint', 'prettier'],
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: ['plugin:vue/essential', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
